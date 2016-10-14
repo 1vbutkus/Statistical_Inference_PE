af2d612470ea471b21a51470b5a1d5a7f26c3c88
Vygantas <vygantas.butkus@gmail.com>
2016-10-14 09:37:15 +0300

    path fixed2

--- Probability1/runCards.R
+++ Probability1/runCards.R
@@ -2,7 +2,7 @@
 # here will show up in the user's workspace when he or she begins
 # the lesson.
 file.copy(from=file.path(find.package("swirl"),
-                         "Courses/Statistical_Inference",
+                         "Courses/Statistical_Inference_PE",
                          "Probability1/cards.R"), 
           to="cards.R")
 # file.edit("cards.R")
